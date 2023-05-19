7cbf3f2f2be87270fe2a8daead36886a59ed6cb4
nsmilyanski <nikola.smilyanski@gmail.com>
2023-05-19 11:25:27 +0300

    add NotifierService

---
+++ src/app/service/notification.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
+
+@Injectable({ providedIn: 'root'})
+export class NotificationService {
+  constructor( private notifier: NotifierService) { }
+
+  public notify(type: string, message: string) {
+    this.notifier.notify(type, message);
+  }
+}
